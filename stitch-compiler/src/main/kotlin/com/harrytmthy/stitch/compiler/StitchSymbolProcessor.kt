7c0fad3d49541b8db305a62f88c6e4631856b00e
harry.tumalewa <harrytmthy@gmail.com>
2026-01-31 02:22:12 +0700

    refactor(compiler): Remove "stitch.moduleKey" and hash moduleName for package suffix

--- stitch-compiler/src/main/kotlin/com/harrytmthy/stitch/compiler/StitchSymbolProcessor.kt
+++ stitch-compiler/src/main/kotlin/com/harrytmthy/stitch/compiler/StitchSymbolProcessor.kt
@@ -46,12 +46,8 @@ class StitchSymbolProcessor(private val environment: SymbolProcessorEnvironment)
 
             if (!registry.isAggregator) {
                 val moduleName = getOption("stitch.moduleName")
-                val moduleKey = getOption("stitch.moduleKey")
-                ContributionCodeGenerator(environment.codeGenerator).generate(
-                    packageSuffix = moduleKey,
-                    fileName = "Generated${moduleName}Contribution",
-                    registry = registry,
-                )
+                ContributionCodeGenerator(environment.codeGenerator)
+                    .generate(moduleName, registry)
             }
         } catch (e: StitchProcessingException) {
             e.message?.let { logger.error(it, e.symbol) }
